2e8c04e4307443e3d79973b2931104bd6b28132b
StevenAlvaradoAguilar <48374564+StevenAlvaradoAguilar@users.noreply.github.com>
2020-03-09 14:44:30 -0600

    Agrego la tarea de convertir un código en C a Ensamblador por medio de IDA Pro.

---
+++ Tarea_Suma/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+  //Declaramos los atributos dentro del main.
+  int numero_1, numero_2, suma; 
+
+  //Imprimimos el mensaje de agregar el primer número.
+  printf("Digite el primer numero: ");
+
+  //Lo escaneamos y guardamos en la memoria de la computadora.
+  scanf("%d", &numero_1); 
+
+  //Imprimimos el mensaje de agregar el segundo número.
+  printf("Digite el segundo numero: ");
+
+  //Lo escaneamos y guardamos en la memoria de lacomputadora.
+  scanf("%d", &numero_2); 
+
+  //Sumamos los dos números y la guardamos en elatributo suma.
+  suma = numero_1 + numero_2;
+
+  //Imprimimos el mensaje del resultado de la suma.
+  printf("La suma de los numeros es de: %d", suma);
+
+  printf("\nMuchas gracias por ejecutar el programa.");
+}
